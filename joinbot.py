ff3029eeeb8076bb6345334fd520bf88751d04d4
Baron9876 <39878874+Baron9876@users.noreply.github.com>
2018-06-11 19:20:44 +0100

    Update joinbot.py

--- joinbot.py
+++ joinbot.py
@@ -14,7 +14,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    fmt = '{0.mention} joined.'
+    fmt = 'Welcome, {0.mention}, enjoy the server and read #rules'
     channel = member.server.get_channel("411967511387570176")
     await bot.send_message(channel, fmt.format(member))
 
